8b3e798c5b6d9da02760f87d8f1ae9abfe30d6ba
AKatelinskaya <anastasiakatelinskaya@gmail.com>
2022-04-17 21:28:49 +0300

    change prime_number.js

--- lw9/prime_number.js
+++ lw9/prime_number.js
@@ -1,12 +1,43 @@
+function isPrimeNumber(el) {
+    let arr = toArrayOfNum(el);
 
-function isPrimeNumber(n) {
-    switch (typeof n) {
-        case "object": {
-
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "object") {
+            isPrimeNumber(arr[i]);
         }
-        case "number": {
-
+        else {
+            if (typeof arr[i] !== "number") {
+                console.log(arr[i], "is unexpected element");
+            } else if (isPrime(arr[i])) {
+                console.log(arr[i], " is prime number");
+            } else {
+                console.log(arr[i], " is not prime number");
+            }
         }
-
     }
+    return "Ok";
+}
+
+function toArrayOfNum(el) {
+    switch (typeof el) {
+        case "object":
+            return el;
+        case "number":
+            return [el];
+        default:
+            console.log(el, "is unexpected element");
+            return [];
+    }
+}
+
+function isPrime(el) { //получаем точно число
+    if ((el >= 2)) {
+        for (let i = 2; i <= el - 1; i++) {
+            if (el % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
 }
